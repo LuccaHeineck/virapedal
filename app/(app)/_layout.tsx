7196bc3638ae49cc47f32adc53c156f457724860
Gustavo Borba <gustavoborbavts@gmail.com>
2026-08-26 20:59:35 -0300

    feat: adiciona aba de eventos e tela de criacao de pedal

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -22,6 +22,22 @@ export default function AppLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="events"
+        options={{
+          title: 'Eventos',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons name={focused ? 'bicycle' : 'bicycle-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create-event"
+        options={{
+          href: null,
+          title: 'Criar Evento',
+        }}
+      />
     </Tabs>
   );
 }
